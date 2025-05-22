7e6bdb58b062ac726fe43d296a89da04081888b2
eddsticks <ewaldo.jefe@gmail.com>
2025-05-22 10:03:25 -0400

    adición LoginTest

---
+++ src/test/java/org/example/LoginTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+class LoginTest {
+
+}
